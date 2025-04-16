c9e29e600c2d5be721e8f92eeac7b89086442239
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2025-04-16 13:49:44 -0400

    Finished styling homepage and navbar

--- app/app/components/Navbar.tsx
+++ app/app/components/Navbar.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import { IoPerson } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <nav className='bg-[#D9D9D933] h-[10vh] w-screen'>
-        <div className='flex flex-row justify-between items-center'>
-            <div className='w-[30vh] flex flex-row items-center mx-4 border'>
+        <div className='flex flex-row items-center justify-between'>
+            <div className='w-[40%] h-full flex flex-row items-center m-4 p-4'>
                 <h1>LOGO HERE</h1>
             </div>
-            <div className='w-[70vh] flex flex-row items-center justify-between mx-4 border'>
+            <div className='w-[60%] h-full flex flex-row items-center justify-between mr-8'>
                 <button className='font-[family-name:var(--font-micro)] text-4xl'>Videos</button>
                 <button className='font-[family-name:var(--font-micro)] text-4xl'>Blog</button>
                 <button className='font-[family-name:var(--font-micro)] text-4xl'>Writing</button>
                 <button className='font-[family-name:var(--font-micro)] text-4xl'>My Media</button>
                 <button className='font-[family-name:var(--font-micro)] text-4xl'>Kofi</button>
+                <IoPerson className="text-2xl"/>
             </div>
         </div>
     </nav>
